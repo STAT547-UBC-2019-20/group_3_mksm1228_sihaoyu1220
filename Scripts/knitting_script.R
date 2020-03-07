138bd3360138c28caa240efe09938d8cf9f7477b
Martin Keung <herrotiga@gmail.com>
2020-03-07 07:31:11 -0800

    knitting script.r

--- Scripts/knitting_script.R
+++ Scripts/knitting_script.R
@@ -1,30 +1,44 @@
 "This scripts knits the draft to an html or pdf file. Please enter the file you 
-wish to convert into the first argument and the file type 'html' or 'pdf' in the second
-argument."
+wish to knit into the first argument and the file type 'html' or 'pdf' in the second
+argument.
 
-Usage: knitting_script.R<file_type>
-doc 
+Usage: knitting_script.R<file, file_type>
+" -> doc 
 
 library(knitr)
 library(docopt)
-
+library(here)
+library(testthat)
 
 opt <- docopt(doc)
 
-main <- function(file, file_type){
+main <- function(file_type){
+  file <- here("Docs", "Milestone 1.Rmd")
   if (file_type == "html"){
-    rmarkdown::render(file, "html_document")
+    rmarkdown::render(file, "html_document", output_dir = "Docs")
   }  
   if (file_type == "pdf"){
-    rmarkdown::render(file, "pdf_document")
+    rmarkdown::render(file, "pdf_document", output_dir = "Docs")
   }
-  else {
-    print("That is not a valid file format. Please enter 'html' or 'pdf.'")
-  }}
+  
+  ### Tests
+  
+  test_that("Correct file type written", {
+    if (file_type == "html"){
+      expect_match("html", file_type)
+    }
+    if (file_type == "pdf"){
+      expect_match("pdf", file_type)
+    } 
+    else {
+      print("Please input 'html' or 'pdf.'")
+    } 
+  })
+  
+  
+}
 
-main(file, opt$file_type)
+main(file_type)
 
-### Tests
-test_that("Check if file output",)
 
 main(opt$file, opt$file_type)
