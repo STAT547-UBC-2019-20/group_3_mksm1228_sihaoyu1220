7c13dbb352ce8abc63c166ad6cecfb42ea023f0b
Sherry Gao <gaosi4749@gmail.com>
2020-03-14 13:50:25 -0700

    update linear_regression.R

--- Scripts/linear_regression.R
+++ Scripts/linear_regression.R
@@ -1,3 +1,3 @@
 "This is a script that will perform a linear regression on the dataset provided.
 Please input the file you wish to perform a linear regression on.
-Usage: "
+Usage: linear_regression.R --data_path=<data_path> --image_path=<image_path>"
